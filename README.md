037234c8c2c17505d0bfa98432ec36149bf613a9
jfseb <jfsebpublic@gmail.com>
2017-03-10 20:12:12 +0100

    commit project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # abot_stringdist
 string distance functions
+
+based on natural
+         talisman
+
---
+++ js/index.d.ts
@@ -0,0 +1,4 @@
+export * from "./model/model";
+export * from "./utils/damerauLevenshtein";
+export * from "./match/ifmatch";
+export declare var bas: string;
